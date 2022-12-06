1fc680bc3f7f7d14757b3ccb30c53395732a9a77
kchaehyeon01 <kchaehyeon01@gmail.com>
2022-12-07 02:21:07 +0900

    221207:chap2

--- 1_Chapter1/image.py
+++ 1_Chapter1/image.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
-img = imread("honeybadger.jpeg")
+img = imread("honeybadger.jpeg") # 이미지 파일이 현재 작업 디렉터리에 있음
 plt.imshow(img)
 plt.show()
+# %%
